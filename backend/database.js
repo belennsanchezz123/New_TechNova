146d0ead6a5c572bf4db5f35b35c7b950cbb6c31
belennsanchezz123 <belen.sanchezp@um.es>
2025-10-30 18:21:26 +0100

    Updated database.js

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,9 @@
-import Database from 'better-sqlite3';
+import { createRequire } from 'module';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+
+const require = createRequire(import.meta.url);
+const Database = require('better-sqlite3');
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
